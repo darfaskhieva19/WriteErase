427a0e0e50581cb7fe343bfcadb42184bdf029a6
daria_faskhieva <dashaf712@mail.ru>
2023-03-17 00:01:45 +0300

    first commit

---
+++ WriteErase/pages/PageAuto.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WriteErase
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAuto.xaml
+    /// </summary>
+    public partial class PageAuto : Page
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        int time = 10;
+        public PageAuto()
+        {
+            InitializeComponent();
+            tbLogin.Focus();
+        }
+        public PageAuto(int k)
+        {
+            InitializeComponent();
+            switch (k)
+            {
+                case 0:
+                    btAuto.IsEnabled = true;
+                    tbLogin.Focus();
+                    spCode.Visibility = Visibility.Collapsed;
+                    break;
+                case 1:
+                    btAuto.IsEnabled = false;
+                    tbLogin.Text = "";
+                    tbPassword.Password = "";
+                    tbLogin.IsEnabled = false;
+                    tbPassword.IsEnabled = false;
+                    tbLogin.Focus();
+                    timer.Interval = new TimeSpan(0, 0, 1);
+                    timer.Tick += new EventHandler(Timer_Tick);
+                    timer.Start();
+                    spCode.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            time--;
+            tbTime.Text = "Вы сможете зайти через " + time + " секунд";
+            if (time < 0)
+            {
+                timer.Stop();
+                btAuto.IsEnabled = true;
+                tbTime.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void tbAuto_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbLogin.Text == "" || tbPassword.Password == "")
+            {
+                MessageBox.Show("Не все обязательные поля заполнены!");
+            }
+            else
+            {
+                User user = DataBase.Base.User.FirstOrDefault(z => z.UserLogin == tbLogin.Text && z.UserPassword == tbPassword.Password);
+                if (user == null)
+                {
+                    MessageBox.Show("Вы ввели данные неверно! Повторите вход!");
+                    btAuto.IsEnabled = false;
+                    CAPTCHA();
+                }
+                else
+                {
+                    CAPTCHA();
+                    switch (user.UserRole)
+                    {
+                        //case 1: //клиент
+                        //    ClassFrame.frameL.Navigate(new ListOfTovar(user));
+                        //    break;
+                        //case 2: //администратор
+                        //    ClassFrame.frameL.Navigate(new ListOfTovar(user));
+                        //    break;
+                        //case 3: //менеджер
+                        //    ClassFrame.frameL.Navigate(new ListOfTovar(user));
+                        //    break;
+                    }
+                }
+            }
+        }
+
+        public void CAPTCHA()
+        {
+            WindowCaptcha captcha = new WindowCaptcha();
+            captcha.ShowDialog();
+        }
+
+        private void tbGuest_Click(object sender, RoutedEventArgs e)
+        {
+            ClassFrame.frameL.Navigate(new ListOfTovar());
+        }
+    }
+}
---
+++ WriteErase/windows/WindowCaptcha.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WriteErase
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowCaptcha.xaml
+    /// </summary>
+    public partial class WindowCaptcha : Window
+    {
+        string str = String.Empty;
+        public WindowCaptcha()
+        {
+            InitializeComponent();
+            Random random = new Random();
+            string sym = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string[] ch = new string[4];
+            for (int i = 0; i < 10; i++)
+            {
+                SolidColorBrush solidColor = new SolidColorBrush(Color.FromRgb((byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256)));
+                Line l = new Line()
+                {
+                    X1 = random.Next((int)CCaptcha.Width),
+                    Y1 = random.Next((int)CCaptcha.Height),
+                    X2 = random.Next((int)CCaptcha.Width),
+                    Y2 = random.Next((int)CCaptcha.Height),
+                    Stroke = solidColor
+                };
+                CCaptcha.Children.Add(l);
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                ch[i] = Convert.ToString(sym[random.Next(sym.Length)]);
+                str += ch[i];
+            }
+            TextBlock txt1 = new TextBlock()
+            {
+                Text = (string)ch[0].ToString(),
+                TextDecorations = TextDecorations.Strikethrough,
+                FontSize = random.Next(18, 22),
+                FontFamily = new FontFamily("Comic Sans MS"),
+                FontWeight = FontWeights.Bold,
+                FontStyle = FontStyles.Italic,
+                Margin = new Thickness(10),
+                Padding = new Thickness(25)
+            };
+            CCaptcha.Children.Add(txt1);
+            TextBlock txt = new TextBlock()
+            {
+                Text = (string)ch[1].ToString(),
+                FontSize = random.Next(14, 18),
+                FontFamily = new FontFamily("Comic Sans MS"),
+                FontWeight = FontWeights.Bold,
+                Margin = new Thickness(15),
+                Padding = new Thickness(30)
+            };
+            CCaptcha.Children.Add(txt);
+            TextBlock txt2 = new TextBlock()
+            {
+                Text = (string)ch[2].ToString(),
+                TextDecorations = TextDecorations.Strikethrough,
+                FontSize = random.Next(14, 18),
+                FontFamily = new FontFamily("Comic Sans MS"),
+                FontWeight = FontWeights.Bold,
+                FontStyle = FontStyles.Italic,
+                Margin = new Thickness(10),
+                Padding = new Thickness(45)
+            };
+            CCaptcha.Children.Add(txt2);
+            TextBlock txt3 = new TextBlock()
+            {
+                Text = (string)ch[3].ToString(),
+                FontSize = random.Next(14, 18),
+                FontFamily = new FontFamily("Comic Sans MS"),
+                FontWeight = FontWeights.Bold,
+                Margin = new Thickness(15),
+                Padding = new Thickness(65)
+            };
+            CCaptcha.Children.Add(txt3);
+        }
+
+        private void tbCaptcha_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tbCaptcha.Text.Length == 4)
+            {
+                if (tbCaptcha.Text == str)
+                {
+                    MessageBox.Show("Успешно!", "Подтверждение");
+                    ClassFrame.frameL.Navigate(new ListOfTovar());
+                    Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("CAPTCHA введена неверно!", "Ошибка");
+                    ClassFrame.frameL.Navigate(new PageAuto(1));
+                    Close();
+                }
+            }
+        }
+    }
+}
